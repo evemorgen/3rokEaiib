aef1222197eb6e6c9693b443ce596cc67ab78a83
Gałczyński Patryk <Patryk.Galczynski@dreamlab.pl>
2016-10-17 15:08:20 +0200

    fixes after 3 labs

--- obrazy/lab3/wykonanie/zadanie2.m
+++ obrazy/lab3/wykonanie/zadanie2.m
@@ -12,11 +12,11 @@ dodany = imadd(lena, jet);
 dodany_uint16 = imadd(lena, jet, 'uint16');
 
 figure;
-subplot(4,2,1);
+subplot(5,2,1);
 imshow(dodany);
 title('uint8');
 
-subplot(3,2,2);
+subplot(5,2,2);
 imshow(dodany_uint16, []);
 title('uint16');
 
@@ -24,11 +24,11 @@ title('uint16');
 %% pkt 4.
 kombinacja_lena = imlincomb(2, lena);
 kombinacja_jet = imlincomb(1.3, jet);
-subplot(4,2,3);
+subplot(5,2,3);
 imshow(kombinacja_lena, []);
 title('kombinacja lena');
 
-subplot(3,2,4);
+subplot(5,2,4);
 imshow(kombinacja_jet, []);
 title('kombinacja jet');
 
@@ -36,24 +36,29 @@ title('kombinacja jet');
 odejete_lena = imsubtract(lena, jet);
 odejete_lena_16 = imsubtract(int16(lena), int16(jet));
 
-subplot(4,2,5);
+subplot(5,2,5);
 imshow(odejete_lena, []);
 title('odjęta 10 lena');
 
-subplot(4,2,6);
+subplot(5,2,6);
 imshow(odejete_lena_16, []);
 title('odjęty 10 lena uint16');
 
-subplot(4,2,7);
+subplot(5,2,7);
 diff = imabsdiff(lena, jet);
 imshow(diff);
 title('imabsdiff');
 
-subplot(4,2,8);
+subplot(5,2,8);
 maska = boolean(imread('kolo.bmp'));
-mull = );
-imshow(diff);
-title('imabsdiff');
+mull = immultiply(lena, maska);
+imshow(mull);
+title('immulltiply');
+
+subplot(5,2,9:10);
+neg = imcomplement(lena);
+imshow(neg);
+title('negatyw');
 
 
 
