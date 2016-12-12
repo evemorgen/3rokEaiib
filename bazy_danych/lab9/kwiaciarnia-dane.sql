fa67e5376bf0559538f13d38d94ffdfd6584da5c
Patryk Gałczyński <evemorgen1911@gmail.com>
2016-12-12 20:00:50 +0100

    lab9 draft

---
+++ bazy_danych/lab9/kwiaciarnia-dane.sql
@@ -0,0 +1,148 @@
+
+set schema 'kwiaciarnia';
+
+truncate table klienci cascade;
+truncate table zamowienia cascade;
+truncate table odbiorcy cascade;
+truncate table kompozycje cascade;
+truncate table zapotrzebowanie cascade;
+truncate table historia cascade;
+
+
+-- klienci
+copy klienci from stdin with (null 'BRAK DANYCH', delimiter ';');
+msowins;msowins;Magdalena Sowinska;Krakow;30-362;ul. Ceglarska 4/101;msowins@wp.pl;(012) 664 46 99;(012) 664 46 99;707-709-12-13;BRAK DANYCH
+mbabik;mbabik;Malgorzata Babik;Krakow;31-466;ul. Akacjowa 4;mbabik@wp.pl;609 101 101;BRAK DANYCH;687-988-66-66;BRAK DANYCH
+mfibak;mfibak;Marta Fibak;Zielonki;32-087;Zielonki 4;mfibak@wp.pl;(012) 622 22 85;(012) 622 22 85;121-657-09-08;BRAK DANYCH
+dniemcz;dniemcz;Damian Niemczyk;Slomniki;32-090;ul. Niecala 9;dniemcz@wp.pl;(012) 444 44 57;(012) 444 44 57;345-675-99-87;BRAK DANYCH
+gurbanik;gurbanik;Grzegorz Urbanik;Wieliczka;32-020;ul. Kwiatowa 33;gurbanik@wp.pl;0 609 506 606;BRAK DANYCH;435-987-45-55;BRAK DANYCH
+pjurecz;pjurecz;Pawel Jureczek;Wieliczka;32-020;ul. Legionow 4/53;pjurecz@wp.pl;(012) 411 18 88;BRAK DANYCH;707-709-12-14;BRAK DANYCH
+msrokiew;msrokiew;Marcin Srokiewicz;Krakow;30-362;ul. Ceglarska 45/12;msrokiew@wp.pl;609 102 102;BRAK DANYCH;687-988-66-67;BRAK DANYCH
+bzameck;bzameck;Beata Zamecka;Krakow;31-450;ul. Ulanow 21/64;bzameck@wp.pl;(012) 634 77 09;BRAK DANYCH;121-657-09-09;BRAK DANYCH
+gkiwi;gkiwi;Gabriela Kiwi;Krakow;31-464;ul. Majowa 45/2;gkiwi@wp.pl;(012) 630 00 12;(012) 630 00 12;345-675-99-88;BRAK DANYCH
+jkajdeck;jkajdeck;Jan Kajdecki;Krzeszowice;32-065;ul. Piastowska 22/45;jkajdeck@wp.pl;(012) 655 77 98;(012) 655 77 98;435-987-45-56;BRAK DANYCH
+acygan;acygan;Andrzej Cygan;Krakow;30-838;ul. Heleny 24/1;acygan@wp.pl;0 609 200 300;BRAK DANYCH;707-709-12-15;BRAK DANYCH
+ztylutek;ztylutek;Zofia Tylutek;Krakow;30-034;ul. Koscielna 45/100;ztylutek@wp.pl;(012) 612 12 56;BRAK DANYCH;687-988-66-68;BRAK DANYCH
+msitarz;msitarz;Marcin Sitarz;Wieliczka;32-020;ul. Kwiatowa 2;msitarz@wp.pl;609 100 101;BRAK DANYCH;121-657-09-10;BRAK DANYCH
+ppsiarek;ppsiarek;Paulina Psiarek;Niepolomice;32-005;ul. Wielicka 56;ppsiarek@wp.pl;0 607 200 201;BRAK DANYCH;345-675-99-89;BRAK DANYCH
+Ilipska;Ilipska;Iwona Lipska;Wieliczka;32-020;ul. Urocza 8/7;Ilipska@wp.pl;(012) 433 55 55;(012) 433 55 55;435-987-45-57;BRAK DANYCH
+mmoraw;mmoraw;Malgorzata Morawiec;Krzeszowice;32-065;ul. Mila 576/5;mmoraw@wp.pl;(012) 633 55 71;BRAK DANYCH;707-709-12-16;BRAK DANYCH
+abazarek;abazarek;Anna Bazarek;Krakow;31-546;ul. Aleja Pokoju 21/23;abazarek@wp.pl;(012) 611 11 23;BRAK DANYCH;687-988-66-69;BRAK DANYCH
+msowin;msowin;Marek Sowinski;Krakow;30-362;ul. Ceglarska 4/101;msowin@wp.pl;(012) 664 46 99;(012) 664 46 99;121-657-09-11;BRAK DANYCH
+mpalka;mpalka;Monika Palka;Krakow;31-335;ul. Chabrowa 43;mpalka@wp.pl;0 502 322 323;BRAK DANYCH;345-675-99-90;BRAK DANYCH
+jmichno;jmichno;Jacek Michno;Wieliczka;32-020;ul. Urocza 85/7;jmichno@onet.pl;(012) 433 55 56;BRAK DANYCH;707-709-12-14;BRAK DANYCH
+llipecki;llipecki;lukasz Lipecki;Wieliczka;32-020;ul. Urocza 7/66;llipecki@onet.pl;605 200 200;BRAK DANYCH;687-988-66-67;BRAK DANYCH
+dkopera;dkopera;Dorota Kopera;Slomniki;32-090;ul. Niecala 6;dkopera@onet.pl;(012) 444 44 56;(012) 444 44 56;121-657-09-09;BRAK DANYCH
+lwitek;lwitek;lukasz Witek;Niepolomice;32-005;ul. Mala 2;lwitek@onet.pl;(012) 666 66 66;(012) 666 66 66;345-675-99-88;BRAK DANYCH
+bzamecka;bzamecka;Barbara Zamecka;Krakow;31-450;ul. Ulanow 21/64;bzamecka@onet.pl;(012) 634 77 09;(012) 634 77 09;435-987-45-56;BRAK DANYCH
+mrusinek;mrusinek;Magdalena Rusinek;Krakow;30-362;ul. Ceglarska 78/6;mrusinek@onet.pl;(012) 664 44 45;BRAK DANYCH;707-709-12-15;BRAK DANYCH
+kkot;kkot;Karol Kot;Krakow;30-383;ul. Obozowa 44/122;kkot@onet.pl;(012) 677 88 80;BRAK DANYCH;687-988-66-68;BRAK DANYCH
+akulfon;akulfon;Anna Kulfon;Krzeszowice;32-065;ul. Mila 22/66;akulfon@onet.pl;(012) 677 88 81;BRAK DANYCH;121-657-09-10;BRAK DANYCH
+ksowa;ksowa;Karolina Sowa;Skawina;32-050;ul. Mila 3;ksowa@onet.pl;(012) 444 44 44;(012) 444 44 44;345-675-99-89;BRAK DANYCH
+azarek;azarek;Artur zarek;Krakow;30-059;ul. Aleja A. Mickiewicza 46/75;azarek@onet.pl;(012) 645 45 48;BRAK DANYCH;435-987-45-57;BRAK DANYCH
+rkapusta;rkapusta;Ryszard Kapusta;Krakow;31-464;ul. Majowa 1;rkapusta@onet.pl;(012) 645 45 49;(012) 645 45 49;707-709-12-16;BRAK DANYCH
+azator;azator;Adam Zator;Krakow;31-450;ul. Ulanow 27/52;azator@onet.pl;(012) 634 77 09;BRAK DANYCH;687-988-66-69;BRAK DANYCH
+gkopicz;gkopicz;Grzegorz Kopiczynski;Wieliczka;32-020;ul. Os. Kosciuszki 12;gkopicz@onet.pl;0 502 400 487;BRAK DANYCH;121-657-09-11;BRAK DANYCH
+kkorzec;kkorzec;Katarzyna Korzecka;Krakow;30-383;ul. Obozowa 57/6;kkorzec@onet.pl;(012) 677 88 88;(012) 677 88 88;345-675-99-90;BRAK DANYCH
+wzakon;wzakon;Waldemar Zakoniecki;Skawina;32-050;ul. Wrzesniowa 44;wzakon@onet.pl;(012) 677 88 89;BRAK DANYCH;435-987-45-58;BRAK DANYCH
+akopicz;akopicz;Anna Kopiczynska;Wieliczka;32-020;ul. Os. Kosciuszki 12;akopicz@onet.pl;0 502 400 487;BRAK DANYCH;707-709-12-17;BRAK DANYCH
+mmisiek;mmisiek;Maciej Misiek;Niepolomice;32-005;ul. Rynek 5/17;mmisiek@onet.pl;(012) 651 51 52;(012) 651 51 52;687-988-66-70;BRAK DANYCH
+kkowal;kkowal;Konrad Kowal;Wieliczka;32-020;ul. OSP Trabki 52;kkowal@onet.pl;(012) 411 15 57;(012) 411 15 57;121-657-09-12;BRAK DANYCH
+kderen;kderen;Karolina Deren;Krakow;30-838;ul. Heleny 5/23;kderen@onet.pl;(012) 632 66 66;(012) 632 66 66;345-675-99-91;BRAK DANYCH
+zkamien;zkamien;Zofia Kamien;Krakow;31-990;ul. Wadowicka 45/22;zkamien@interia.pl;(012) 633 88 88;BRAK DANYCH;707-709-12-14;BRAK DANYCH
+pzegadlo;pzegadlo;Piotr Zegadlo;Bochnia;32-701;ul. Wygoda 13/13;pzegadlo@interia.pl;(014) 557 55 66;BRAK DANYCH;687-988-66-67;BRAK DANYCH
+pkoper;pkoper;Pawel Koper;Krakow;30-650;ul. Makowa 1/19;pkoper@interia.pl;0 608 350 334;BRAK DANYCH;121-657-09-09;BRAK DANYCH
+mkon;mkon;Mariola Kon;Bochnia;32-701;ul. Wygoda 29/1;mkon@interia.pl;0 502 100 222;BRAK DANYCH;345-675-99-88;BRAK DANYCH
+rrusinek;rrusinek;Robert Rusinek;Krakow;30-362;ul. Ceglarska 78/6;rrusinek@interia.pl;(012) 664 44 45;BRAK DANYCH;435-987-45-56;BRAK DANYCH
+posa;posa;Paulina Osa;Skawina;32-050;ul. Krakowska 4;posa@interia.pl;(012) 442 78 78;(012) 442 78 78;707-709-12-15;BRAK DANYCH
+aenglert;aenglert;Anna Englert;Krakow;31-990;ul. Wadowicka 3/67;aenglert@interia.pl;(012) 611 22 44;(012) 611 22 44;687-988-66-68;BRAK DANYCH
+koral;koral;FHU Koral;Bochnia;32-700;ul. Legionow Polskich 23/11;koral@interia.pl;(014) 611 77 77;(014) 611 77 77;BRAK DANYCH;590096454
+caro;caro;SC Caro;Wieliczka;32-020;ul. Kwiatowa 77;caro@interia.pl;(012) 402 21 65;BRAK DANYCH;BRAK DANYCH;650043357
+amanda;amanda;Sklep Amanda;Krakow;31-406;ul. Aleja 29 Listopada 153/88;amanda@interia.pl;0 607 222 345;BRAK DANYCH;BRAK DANYCH;709990260
+sp17;sp17;SP nr 17;Bochnia;32-700;ul. Kazimierza Wielkiego 4/67;sp17@interia.pl;(014) 611 77 63;BRAK DANYCH;BRAK DANYCH;769937163
+karolina;karolina;HFU Karolina;Wieliczka;32-020;ul. Legionow 4/53;karolina@interia.pl;(012) 411 18 88;(012) 411 18 88;BRAK DANYCH;829884066
+\.
+
+-- kompozycje
+copy kompozycje from stdin with (null '', delimiter ';');
+buk1;Bukiet 1;roze, trachelium, jagodzina, ti-tin, fizelina;95.00;3;3
+buk2;Bukiet 2;rozyczki rodeo i inne kwiaty, fizelina;75.00;3;4
+buk3;Bukiet 3;gozdziki, margerytki, santini, kapustka, jagodzina, magic mushrums, papier;90.00;3;4
+ko1;Kosz 1;21 roz pomaranczowych, duzo zieleni i kosz z palakiem;250.00;4;4
+ko2;Kosz rozyczek;tuzin czerwonych rozyczek, molucella, gips, sizal, koszyk czerwony z palakiem;120.00;10;12
+ko3;Kosz mix;gladiole, gerbery, sloneczniki mini, leuki, kolorowe liscie, kosz z palakiem;250.00;4;5
+susz;Kompozycja suszona;suszone i preparowane owoce, warzywa i czesci roslin;70.00;2;2
+ko4;Koszyk gozdzikowy;gozdziki mini w roznych kolorach, gips, molucella, zatrwian, koszyk z kolnierzem;95.00;3;4
+ko5;Hiacynty w koszyku;kosz wiklinowy z hiacyntami i przybraniem;110.00;4;4
+don1;Biala azalia;biala azalia z dekoracja w koszyku;120.00;4;4
+don2;Rozowa azalia;rozowa azalia z dekoracja w koszyku;120.00;4;5
+kw1;Orchidea rozowa;galaz cymbidium, zielona dekoracja, patyki, liscie;95.00;3;4
+ko6;Kosz z azalia;azalia, aglomena, wstazka, sizal, kosz z palakiem;70.00;3;3
+buk4;Wiazanka czerwona;11 roz Amor i przybranie;90.00;8;9
+buk5;Bukiecik serc;3 czerwone rozyczki, serduszka, zielen, podklad sizalowy;75.00;15;16
+kw2;Ikebana z rozami;czerwone rozyczki midi, kwiaty sezonowe, ikebana;50.00;6;7
+ko7;Kwitnacy kosz;cyklamen, hiacynty, ST. Paula, bluszcze, kosz z palakiem, dekoracja;115.00;4;4
+kw3;Sloneczniki z lilia;lilie orientalne, sloneczniki, zeniszek, assengai, mulumbecia, dzbanuszek;100.00;4;4
+kw4;Strelicje;5 strelicji, mandzuria, wazon;240.00;4;5
+kw5;Anturium latem;3 anturia, kapusta, knofia, roza herbaciana, margerytka, pteris;145.00;5;5
+\.
+
+-- odbiorcy
+copy odbiorcy from stdin with (null '', delimiter ';');
+Slawomir Zeganek;Krakow;30-059;Al A. Mickiewicza 4/3
+Dorota Pszczolka;Slomniki;32-090;ul. Wiosenna 8
+Wioletta Barszcz;Skawina;32-050;ul. zwirki i Wigury 7/11
+Monika Kotarek;Wieliczka;32-020;ul. OSP Trabki 5
+Wojciech Skalecki;Skawina;32-050;ul. Krakowska 34
+Malgorzata Mis;Wieliczka;32-020;ul. Krakowska 2/6
+Piotr Tynski;Krakow;31-235;ul. Banacha 3/4
+Pawel Engel;Krzeszowice;32-065;ul. Daszynskiego 7/55
+Malgorzata Korbicz;Krzeszowice;32-065;ul. Piastowska 55/4
+Jakub Pionek;Wieliczka;32-020;ul. Krakowska 44/12
+Katarzyna Nawarek;Niepolomice;32-005;ul. Wielicka 22
+Konrad Misiek;Niepolomice;32-005;ul. Rynek 5/17
+Monika Bazarek;Krakow;31-546;ul. Aleja Pokoju 21/23
+Anna Bykowska;Krakow;30-145;ul. Hamernia 25/11
+Paulina Sitarz;Wieliczka;32-020;ul. Kwiatowa 2
+Joanna Galecki;Krzeszowice;32-065;ul. Krakowska 44/23
+Justyna Kiwi;Krakow;31-464;ul. Majowa 45/2
+Marcin Rympinski;Niepolomice;32-005;ul. Mala 76
+Adam Gierka;Bochnia;32-701;ul. Wygoda 13/12
+Grzegorz Fibak;Zielonki;32-087;Zielonki 4
+Mariusz Gancarz;Wieliczka;32-020;ul. Legionow 2/12
+Michal Knyszecka;Krakow;30-650;ul. Makowa 7/44
+Pawel Skalecki;Skawina;32-050;ul. Krakowska 34
+Marcin Szarotka;Skawina;32-050;ul. Wrzesniowa 1
+Marcin Kod;Krakow;30-650;ul. Makowa 12/47
+Magdalena Szostek;Krakow;30-034;ul. Koscielna 11/88
+lukasz Krawiec;Krzeszowice;32-065;ul. Piastowska 79/4
+\.
+
+-- zamowienia
+copy zamowienia from stdin with (null 'BRAK DANYCH', delimiter ';');
+2703001;msowins;1;buk1;2007-04-27;95.00;n;dolaczyc zyczenia urodzinowe
+2703002;mbabik;1;buk2;2007-04-27;75.00;n;dolaczyc zyczenia urodzinowe
+2703003;mfibak;2;buk3;2007-04-27;90.00;t;dostarczyc po godzinie 17.00
+2803001;dniemcz;3;ko1;2007-04-28;250.00;t;BRAK DANYCH
+2803002;gurbanik;3;ko2;2007-04-29;120.00;t;dostarczyc na godzine 10.30
+2803003;pjurecz;5;ko3;2007-04-29;250.00;n;dolaczyc zyczenia imieninowe
+2803004;msrokiew;6;susz;2007-04-29;70.00;n;BRAK DANYCH
+2803005;bzameck;8;ko4;2007-04-29;95.00;t;dolaczyc zyczenia imieninowe
+2903001;gkiwi;10;ko5;2007-04-29;110.00;n;dolaczyc zyczenia imieninowe
+2903002;jkajdeck;2;don1;2007-04-30;120.00;t;BRAK DANYCH
+2903003;acygan;12;buk2;2007-04-30;75.00;t;BRAK DANYCH
+2903004;ztylutek;9;buk3;2007-04-30;90.00;n;dostarczyc na godzine 10.30
+2903005;amanda;9;ko1;2007-04-30;250.00;n;dostarczyc na godzine 12.30
+2903006;sp17;10;ko2;2007-05-12;120.00;t;dostarczyc na godzine 10.00
+2903007;karolina;15;ko3;2007-05-12;250.00;t;dostarczyc na godzine 11.30
+\.
+
+-- historia
+copy historia from stdin with (null '', delimiter ';');
+2904007;karolina;ko3;250.00;2007-04-12
+2904006;karolina;ko2;120.00;2007-04-12
+2904005;karolina;ko2;120.00;2007-04-22
+2904003;acygan;buk2;75.00;2007-03-30
+2904002;acygan;buk2;75.00;2007-03-29
+2904001;lwitek;buk2;75.00;2007-03-29
+\.
