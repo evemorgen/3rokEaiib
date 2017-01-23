a8aa33784fc3d468afa3c826da764633c994e35a
evemorgen <evemorgen1911@gmail.com>
2017-01-23 14:39:03 +0100

    zad D

--- obrazy/lab13/wykonianie/zadanieD.m
+++ obrazy/lab13/wykonianie/zadanieD.m
@@ -1,4 +1,4 @@
-%% 1. czyścimy
+%% 1. czy??cimy
 close all;
 clearvars;
 clc;
@@ -10,11 +10,13 @@ flena = fft2(lena);
 shift = fftshift(flena);
 zadaniA(obraz);
 
+%%
 [f1, f2] = freqspace(512, 'meshgrid');
 Hd = ones(512);
 
 r = sqrt(f1.^2 + f2.^2);
-Hd((r<0.1)) = 0;
+Hd((r>0.1)) = 0;
+
 figure;
 colormap(jet(64));
 mesh(f1,f2,Hd);
@@ -22,5 +24,74 @@ mesh(f1,f2,Hd);
 przefiltrowany = shift.*Hd;
 bezshiftwa = ifftshift(przefiltrowany);
 beztransformaty = ifft2(bezshiftwa);
+figure('name', 'dolno');
+imshow(beztransformaty, []);
+
+%%
+[f1, f2] = freqspace(512, 'meshgrid');
+Hd = ones(512);
+
+r = sqrt(f1.^2 + f2.^2);
+Hd((r<0.1)) = 0;
+
 figure;
+colormap(jet(64));
+mesh(f1,f2,Hd);
+
+przefiltrowany = shift.*Hd;
+bezshiftwa = ifftshift(przefiltrowany);
+beztransformaty = ifft2(bezshiftwa);
+figure('name', 'g?rno');
+imshow(beztransformaty, []);
+
+
+%%
+[f1, f2] = freqspace(512, 'meshgrid');
+Hd = ones(512);
+
+r = sqrt(f1.^2 + f2.^2);
+Hd((r<0.1 | r>0.9)) = 0;
+
+figure;
+colormap(jet(64));
+mesh(f1,f2,Hd);
+
+przefiltrowany = shift.*Hd;
+bezshiftwa = ifftshift(przefiltrowany);
+beztransformaty = ifft2(bezshiftwa);
+figure('name', 'pasmowo');
+imshow(beztransformaty, []);
+
+%%
+h = fwind1(Hd,hanning(21));
+[H f1 f2] = freqz2(h,512,512);
+
+r = sqrt(f1.^2 + f2.^2);
+H((r<0.1)) = 0;
+
+figure;
+colormap(jet(64));
+mesh(f1,f2,H);
+
+przefiltrowany = shift.*H;
+bezshiftwa = ifftshift(przefiltrowany);
+beztransformaty = ifft2(bezshiftwa);
+figure('name', 'hanning');
+imshow(beztransformaty, []);
+
+%%
+h = fwind1(Hd,chebwin(21));
+[H f1 f2] = freqz2(h,512,512);
+
+r = sqrt(f1.^2 + f2.^2);
+H((r<0.1)) = 0;
+
+figure;
+colormap(jet(64));
+mesh(f1,f2,H);
+
+przefiltrowany = shift.*H;
+bezshiftwa = ifftshift(przefiltrowany);
+beztransformaty = ifft2(bezshiftwa);
+figure('name', 'chebwin');
 imshow(beztransformaty, []);
