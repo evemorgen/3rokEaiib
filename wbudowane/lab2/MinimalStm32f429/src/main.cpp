d7afdae7c08efc3d4cabaa3dc6b09300ea24e02e
evemorgen <evemorgen1911@gmail.com>
2017-03-29 23:10:45 +0200

    minimal c++ stm32f429 project

---
+++ wbudowane/lab2/MinimalStm32f429/src/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+
+#include "Timer.h"
+#include "BlinkLed.h"
+
+#define BLINK_PORT_NUMBER         (6)
+#define BLINK_PIN_NUMBER_GREEN    (13)
+#define BLINK_PIN_NUMBER_RED      (14)
+#define BLINK_ACTIVE_LOW          (false)
+
+BlinkLed blinkLeds[2] =
+  {
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_GREEN, BLINK_ACTIVE_LOW },
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_RED, BLINK_ACTIVE_LOW },
+  };
+
+BlinkLed green_led = {BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_GREEN, BLINK_ACTIVE_LOW};
+
+int main(int argc, char* argv[])
+{
+  Timer timer;
+  timer.start ();
+
+  green_led.powerUp();
+  while(1)
+  {
+	  green_led.turnOn();
+	  timer.sleep (Timer::FREQUENCY_HZ);
+	  green_led.turnOff();
+	  timer.sleep (Timer::FREQUENCY_HZ);
+  }
+  return 0;
+}
